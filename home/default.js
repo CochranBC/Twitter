e81e595c7a25feb978817df58a3fc3d8bfe04d3e
CochranBC <briancochran85@gmail.com>
2016-05-30 16:11:04 -0700

    added data.#1

---
+++ home/default.js
@@ -0,0 +1,17 @@
+var reviews = [
+  {
+    name: 'Starbucks',
+    reviewer: 'Tom',
+    review: 'I enjoy coming here because the staff remembers me by name and they are very courteous. The place is clean and well kept'
+  },
+  {
+    name: 'La Salsa',
+    reviewer: 'Brittney',
+    review: 'They have a wide selection of salsas to choose from. My favorite is the avacado. It gets pretty busy for lunch time, so I like the fact that I can call ahead and place my order. Then I do not have to wait in line.'
+  },
+  {
+    name: 'Discount Tire',
+    reviewer: 'Sam',
+    review: 'Buying tires can be pretty expensive if you come here. The upside is purchasing the warranty. Then whenever something happens to your tire, you can get it replaced for a reasonable price.'
+  }
+]
