1c11f1f53e6ba069ad255261dda4aa74fc98ea10
CochranBC <briancochran85@gmail.com>
2016-06-09 11:35:02 -0700

    Created form and locations array. #5

---
+++ home/locations.js
@@ -0,0 +1,104 @@
+var locations = [
+  {
+    business: 'Saddle Ranch',
+    addressOne: '1870 Harbor Blvd',
+    addressTwo:
+    city: 'Costa Mesa',
+    state: 'CA',
+    zip: '92627',
+    phone: '(949) 651-8760',
+    website: 'http://www.thesaddleranch.com/',
+    category: 'Traditional American', 'Steakhouse'
+  },
+  {
+    business: 'Taco Bell',
+    addressOne: '3010 El Camino Real',
+    addressTwo:
+    city: 'Tustin',
+    state: 'CA',
+    zip: '92782',
+    phone: '(714) 832-1767',
+    website: 'https://www.tacobell.com/',
+    category: 'Fast Food', 'Mexican', 'Tex-Mex',
+  },
+  {
+    business: 'Chase',
+    addressOne: '3978 Barranca Pkwy',
+    addressTwo:
+    city: 'Irvine',
+    state: 'CA',
+    zip: '92606',
+    phone: '(949) 559-5072',
+    website: 'https://www.chase.com/',
+    category: 'Banks'
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// The long way.
+
+// var starOne = document.createElement('span');
+// if (data.score >= 1) {
+//   var theClass = 'fa-star';
+// } else {
+//   var theClass = 'fa-star-o';
+// }
+// starOne.setAttribute('class', 'fa ' + theClass + ' fa-3x');
+// starOne.setAttribute('id', '1');
+// var starTwo = document.createElement('span');
+// if (data.score >= 2) {
+//   var theClass = 'fa-star';
+// } else {
+//   var theClass = 'fa-star-o';
+// }
+// starTwo.setAttribute('class', 'fa ' + theClass + ' fa-3x');
+// starTwo.setAttribute('id', '2');
+//
+// if (data.score >= 3) {
+//   var theClass = 'fa-star';
+// } else {
+//   var theClass = 'fa-star-o';
+// }
+// var starThree = document.createElement('span');
+// starThree.setAttribute('class', 'fa ' + theClass + ' fa-3x');
+// starThree.setAttribute('id', '3');
+//
+// if (data.score >= 4) {
+//   var theClass = 'fa-star';
+// } else {
+//   var theClass = 'fa-star-o';
+// }
+// var starFour = document.createElement('span');
+// starFour.setAttribute('class', 'fa ' + theClass + ' fa-3x');
+// starFour.setAttribute('id', '4');
+//
+// if (data.score >= 5) {
+//   var theClass = 'fa-star';
+// } else {
+//   var theClass = 'fa-star-o';
+// }
+// var starFive = document.createElement('span');
+// starFive.setAttribute('class', 'fa ' + theClass + ' fa-3x');
+// starFive.setAttribute('id', '5');
